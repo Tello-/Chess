2263407a7a6c2d7dde33aaa2848a6b8e5e083a51
Tello- <lollisjosh@gmail.com>
2020-05-27 15:30:13 -0700

    updated some code formatting

--- Chess/Chess.h
+++ Chess/Chess.h
@@ -3,32 +3,32 @@
 
 struct Piece
 {
-	int ownerID{ 0 };
-	char logo{ ' ' };
+	int			ownerID{ 0 };
+	char			logo{ ' ' };
 	// TODO implement move pattern somehow
 };
 
 class Chess
 {
 public:
-	Chess();
-	int Run();
+				Chess();
+	int			Run();
 	
 private:
-	void reset();
-	void input();
-	void p1_input(); // called during State::PLAYER1
-	void p2_input(); // called during State::PLAYER2
-	void update();
-	void render();
-	void drawBoard(char pieces[64]); // Putting this here, no need for a Board.h class yet
+	void			reset();
+	void			input();
+	void			p1_input(); // called during State::PLAYER1
+	void			p2_input(); // called during State::PLAYER2
+	void			update(); // update internal state based on input
+	void			render();
+	void			drawBoard(char pieces[64]); // Putting this here, no need for a Board.h class yet
 
-	bool validateMove(/*TODO: Fill in some paramters to validate*/);
+	bool			validateMove(/*TODO: Fill in some paramters to validate*/);
 
-	enum State {PLAYER1, PLAYER2};
-	State current;
+	enum State	{PLAYER1, PLAYER2};
+	State		current;
 
-	bool gameFinished;
+	bool			gameFinished;
 };
 
 
