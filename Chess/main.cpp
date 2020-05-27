ba8b0686937ddce8bce341d5aa36af15d22c5536
Tello- <lollisjosh@gmail.com>
2020-05-27 13:52:17 -0700

    moved drawboard into board class files

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -25,30 +25,3 @@ int main()
 	return 0;
 }
 
-void DrawBoard(char pieces[64])
-{
-
-	std::string full_bar{ "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" };
-	std::string dotted_bar{ "::       ::       ::       ::       ::       ::       ::       ::       ::" };
-	std::string open_bar{ "::   " };
-	std::string close_bar{ "   ::" };
-
-
-	std::cout << full_bar << std::endl;
-	for (int iRow = 0, iPiece=0; iRow < 8; ++iRow)
-	{
-		std::cout << dotted_bar	<< std::endl;
-		std::cout << dotted_bar	<< std::endl;
-		
-		for (int iCol = 0; iCol < 8; ++iCol)
-		{
-			iCol == 0 ? std::cout << open_bar : std::cout << "   ";
-			std::cout  << pieces[iPiece] << close_bar;
-			++iPiece;
-		}
-		std::cout << std::endl;
-		std::cout << dotted_bar	<< std::endl;
-		std::cout << dotted_bar	<< std::endl;
-		std::cout << full_bar	<< std::endl;
-	}
-}
